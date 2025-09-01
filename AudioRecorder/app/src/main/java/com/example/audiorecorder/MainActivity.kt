7e1530791abbbe9ddc7d4ad6d01a4cffd5b0bfaf
Akshit Sharma <akshitsharma196@gmail.com>
2025-09-01 16:54:22 -0400

    can record permissions

--- AudioRecorder/app/src/main/java/com/example/audiorecorder/MainActivity.kt
+++ AudioRecorder/app/src/main/java/com/example/audiorecorder/MainActivity.kt
@@ -5,8 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+
+const val REQUEST_CODE = 200
 
 class MainActivity : AppCompatActivity() {
+
+    private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private var permissionGranted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        permissionGranted = ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED
+
+        if(!permissionGranted)
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == REQUEST_CODE){
+            permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    private fun startRecording(){
+        if(!permissionGranted){
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+            return
+        }
+        
     }
 }
